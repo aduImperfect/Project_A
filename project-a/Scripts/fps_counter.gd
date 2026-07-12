c9d94502df3ff5e96ce048ee544d16b701943f44
Aditya Subramanian <adu101992@gmail.com>
2026-07-12 17:17:20 +0530

    Shifting scripts out from wrong path and also adding a release focus for textedit example - fps counter to see if it loses focus - testing in progress.

---
+++ project-a/Scripts/fps_counter.gd
@@ -0,0 +1,18 @@
+extends TextEdit
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _input(event: InputEvent):
+	# Check if a mouse button is clicked while the node has focus
+	if has_focus() and event is InputEventMouseButton and event.pressed:
+		# If the click position is outside the node's rectangle
+		if not get_global_rect().has_point(event.position):
+			release_focus()
