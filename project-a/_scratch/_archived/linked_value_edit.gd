64e278123cd6c8bb33976d4197608e07ba20fdd0
Aditya Subramanian <adu101992@gmail.com>
2026-08-11 16:22:07 +0530

    Commented out all code inside linked_value_edit.gd - and game runs fine everytime. As that was the underlying culprit that was causing godot to break - even though this code is inside the _scratch/archived/ folder - it was still being considered in the filesystem cache creation tied with LinkedSpinBox. Have also tested on my end by nuking the file out completely and game runs everytime as well but kept it for it to be tested more just in case.

--- project-a/_scratch/_archived/linked_value_edit.gd
+++ project-a/_scratch/_archived/linked_value_edit.gd
@@ -1,39 +1,39 @@
-class_name LinkedSpinBox
-extends SpinBox
-
-## Name of the autoload singleton to read/write, e.g. "InputsData"
-@export var target_autoload: String = "InputsData"
-## Property on that autoload to bind to, e.g. "max_move_speed"
-@export var target_property: String = ""
-
-var _target: Object
-var _timer_accum: float = 0.0
-var _timer_max: float = 0.5
-var _synced: bool = false
-
-func _ready() -> void:
-	_target = get_node("/root/%s" % target_autoload)
-	_timer_accum = 0.0
-	_synced = false
-
-func _process(delta: float) -> void:
-	if not _synced:
-		_timer_accum += delta
-		if _timer_accum > _timer_max:
-			_timer_accum = 0.0
-			value = _target.get(target_property)
-			_synced = true
-		return
-
-	if InputsData.begin_delay:
-		_synced = false
-	else:
-		_write_back(value)
-
-func _write_back(new_value) -> void:
-	_target.set(target_property, new_value)
-
-func _input(event: InputEvent) -> void:
-	if has_focus() and event is InputEventMouseButton and event.pressed:
-		if not get_global_rect().has_point(event.position):
-			release_focus()
+#class_name LinkedSpinBox
+#extends SpinBox
+#
+### Name of the autoload singleton to read/write, e.g. "InputsData"
+#@export var target_autoload: String = "InputsData"
+### Property on that autoload to bind to, e.g. "max_move_speed"
+#@export var target_property: String = ""
+#
+#var _target: Object
+#var _timer_accum: float = 0.0
+#var _timer_max: float = 0.5
+#var _synced: bool = false
+#
+#func _ready() -> void:
+	#_target = get_node("/root/%s" % target_autoload)
+	#_timer_accum = 0.0
+	#_synced = false
+#
+#func _process(delta: float) -> void:
+	#if not _synced:
+		#_timer_accum += delta
+		#if _timer_accum > _timer_max:
+			#_timer_accum = 0.0
+			#value = _target.get(target_property)
+			#_synced = true
+		#return
+#
+	#if InputsData.begin_delay:
+		#_synced = false
+	#else:
+		#_write_back(value)
+#
+#func _write_back(new_value) -> void:
+	#_target.set(target_property, new_value)
+#
+#func _input(event: InputEvent) -> void:
+	#if has_focus() and event is InputEventMouseButton and event.pressed:
+		#if not get_global_rect().has_point(event.position):
+			#release_focus()
