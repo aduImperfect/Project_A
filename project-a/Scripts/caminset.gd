23b09951184f62aa53e098aad913936dda609a74
Aditya Subramanian <adu101992@gmail.com>
2026-09-17 23:03:27 +0530

    Better inset and camera movement - now based on max_move_speed and max_jump_speed

--- project-a/Scripts/caminset.gd
+++ project-a/Scripts/caminset.gd
@@ -5,7 +5,7 @@ class_name CameraInset
 @export var normalTex : Texture2D
 @export var highlightedTex : Texture2D
 
-@export var size : Vector2
+static var insetSize : Vector2
 #@export var cam_speed : float
 
 static var insetSet : bool
@@ -14,7 +14,7 @@ static var playerPos : Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	insetSize = Vector2(100.0, 100.0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
@@ -27,50 +27,49 @@ func _process(_delta: float) -> void:
 		insetSet = true
 		return
 
-	playerPos = playerNode.get_child(0).global_position
-	get_child(0).scale = size
-
 	if (owner.global_position.x < -9000) || (owner.global_position.y < -9000):
 		return
 
-	#print(owner.name)
+	playerPos = playerNode.get_child(0).global_position
+	
+	print(insetSize)
+	get_child(0).scale = insetSize
 
 	# LEFT INSET
-	if (playerPos.x <= (global_position.x - (size.x / 2))):
+	if (playerPos.x <= (global_position.x - (insetSize.x / 2))):
 		get_child(0).get_child(0).texture = highlightedTex
-		global_position.x += _delta * InputsData.move_speed
-		CameraHelper.position_cam.x += _delta * InputsData.move_speed
+		global_position.x -= _delta * InputsData.max_move_speed
+		CameraHelper.position_cam.x -= _delta * InputsData.max_move_speed
 		#print("Player past left inset. Player at: ", playerPos, " and inset at ", global_position)
-		
 	else:
 		get_child(0).get_child(0).texture = normalTex
 		CameraHelper.position_cam.x -= 0.0
 
 	# RIGHT INSET
-	if (playerPos.x >= (global_position.x + (size.x / 2))):
+	if (playerPos.x >= (global_position.x + (insetSize.x / 2))):
 		get_child(0).get_child(1).texture = highlightedTex
-		global_position.x += _delta * InputsData.move_speed
-		CameraHelper.position_cam.x += _delta * InputsData.move_speed
+		global_position.x += _delta * InputsData.max_move_speed
+		CameraHelper.position_cam.x += _delta * InputsData.max_move_speed
 		#print("Player past right inset. Player at: ", playerPos, " and inset at ", global_position)
 	else:
 		get_child(0).get_child(1).texture = normalTex
 		CameraHelper.position_cam.x += 0.0
 
 	# TOP INSET
-	if (playerPos.y <= (global_position.y - (size.y / 2))):
+	if (playerPos.y <= (global_position.y - (insetSize.y / 2))):
 		get_child(0).get_child(2).texture = highlightedTex
-		global_position.y -= _delta * InputsData.jump_speed
-		CameraHelper.position_cam.y -= _delta * InputsData.jump_speed
+		global_position.y -= _delta * InputsData.max_jump_speed
+		CameraHelper.position_cam.y -= _delta * InputsData.max_jump_speed
 		#print("Player past top inset. Player at: ", playerPos, " and inset at ", global_position)
 	else:
 		get_child(0).get_child(2).texture = normalTex
 		CameraHelper.position_cam.y -= 0.0
 
 	# BOTTOM INSET
-	if (playerPos.y >= (global_position.y + (size.y / 2))):
+	if (playerPos.y >= (global_position.y + (insetSize.y / 2))):
 		get_child(0).get_child(3).texture = highlightedTex
-		global_position.y += _delta * InputsData.jump_speed
-		CameraHelper.position_cam.y += _delta * InputsData.jump_speed
+		global_position.y += _delta * InputsData.max_jump_speed
+		CameraHelper.position_cam.y += _delta * InputsData.max_jump_speed
 		#print("Player past bottom inset. Player at: ", playerPos, " and inset at ", global_position)
 	else:
 		get_child(0).get_child(3).texture = normalTex
